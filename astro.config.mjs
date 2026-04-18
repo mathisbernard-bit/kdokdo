bda7f23ff6a1bcd2eed1ac390530c90107480c05
mathisbernard-bit <mathis.bernard@grow-lot.com>
2026-04-18 13:45:30 +0200

    feat: ajout sitemap plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://kdokdo.com',
-  integrations: [mdx()],
+  integrations: [
+    mdx(),
+    sitemap(),
+  ],
 });
